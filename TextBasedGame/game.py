e7c6101ecd2219545f53b4ba93f12d8d4054f050
Sobajer <68204977+Jlep243@users.noreply.github.com~>
2025-01-04 02:40:56 -0700

    Choice of where to go in the city center now works.

--- TextBasedGame/game.py
+++ TextBasedGame/game.py
@@ -10,7 +10,7 @@ def game():
 
     def city_center():#This is where the player decides to go
         #Beginning of the game
-        print("You wake up to the blue of the sky and feel the warmth and grainy sands under you")
+        print("You wake up to the blue of the sky and feel the warmth of the grainy sands under you")
         print("You sit up and see a beach ahead of you.")
         travel_direction = input("you look around you and see a trail that goes north and another that goes east\n")
 
@@ -19,16 +19,20 @@ def game():
             print("you arrive at a town and see that there is \n")
             print("pick a number")
             city_choice = input("""There are multiple buildings that you can go to.\n 1.Cathedral \n 2.Tavern \n 3.Manor\n""")
-            if city_choice == 1:
+            if city_choice == "1":
                 cathedral()
-            elif city_choice == 2:
+            elif city_choice == "2":
                 Tavern()
-            elif city_choice == 3:
+            elif city_choice == "3":
                 Manor()
+            return
         elif travel_direction == "south":
             print("you arrive at a small tribe village \n")
             print("pick a number")
             city_choice = input("""There are places you can visit. \n 1.Village bonfire \n 2. Ritual hut \n 3.Drinking hut \n""")
+            return
+
+        
 
     city_center()
 
