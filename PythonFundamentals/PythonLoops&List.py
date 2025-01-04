158a396a3f0451d9727b830b2d023e4929fea5c9
Sobajer <68204977+Jlep243@users.noreply.github.com~>
2025-01-03 19:06:21 -0700

    added information about for loops and list

---
+++ PythonFundamentals/PythonLoops&List.py
@@ -0,0 +1,7 @@
+#For Loop
+
+fruits = ["Apple","Peach","Pear"] #We have a list of fruits. Lists store multiple items in a single variable. They are also indexed and start at 0
+
+for fruit in fruits:
+    """This is a For Loop and this for loop will print each """
+    print(fruit)
