36645a55bd314faeb595bcb9e4ab1a669d0ed35c
Sobajer <68204977+Jlep243@users.noreply.github.com~>
2025-01-03 19:36:44 -0700

    Updated info

--- PythonFundamentals/PythonLoops&List.py
+++ PythonFundamentals/PythonLoops&List.py
@@ -1,7 +1,11 @@
 #For Loop
 
 fruits = ["Apple","Peach","Pear"] #We have a list of fruits. Lists store multiple items in a single variable. They are also indexed and start at 0
+x = 0
 
 for fruit in fruits:
-    """This is a For Loop and this for loop will print each """
+    """This is a For Loop and this for loop will print each item in the fruits list. It will also increment the variable of 
+    x by one each time it loops"""
     print(fruit)
+    x += 1 
+    print(x)
