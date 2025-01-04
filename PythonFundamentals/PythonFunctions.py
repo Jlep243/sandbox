32cad4c197afc0275010c580068a6fc54510958c
Sobajer <68204977+Jlep243@users.noreply.github.com~>
2025-01-03 21:49:43 -0700

    Added info about python functions

---
+++ PythonFundamentals/PythonFunctions.py
@@ -0,0 +1,17 @@
+"""In this section we go over Functions that we can build but there are also built in functions"""
+#Like so -----> https://docs.python.org/3/library/functions.html
+
+#all functions work the same. They all have a name and parentheses 
+#like so --> print(), len(),
+
+#We could also make our own function. 
+#We start with def my_function_name():
+
+def my_function():
+    print("hello")
+    print("bye")
+
+#Though when we make a function. It will not run by its own. 
+#We need to call the function like we do with print function or the length function
+
+my_function() 
