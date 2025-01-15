df743bcb7a7b6be9bd0a7d89a5c793317672d96c
Jerry <68204977+Jlep243@users.noreply.github.com>
2025-01-14 20:40:21 -0700

    fixes toward the choice function

--- PythonCoffeeMachine/main.py
+++ PythonCoffeeMachine/main.py
@@ -3,11 +3,15 @@ from menu import MENU
 from menu import resources
 
 def transaction(choice):
+    #first implementation of function
     print(f"Please insert coins. That will be ${MENU[choice]["cost"]}")
+    #We need to figure out how to add these and give back the change
+    #as well as give the coffee
     quarters = input("how many quarters?")
-    dimes = input("how many dimes?")
+    dimes = input("how many dimes?") 
     nickels = input("how many nickels?")
     pennies = input("how many pennies?")
+    
 
 def resource(choice):
     #Resources needed to make the drink chosen
@@ -26,14 +30,13 @@ def resource(choice):
 #function for picking coffee
 def coffee_choices():
     choice = input("Select a coffee: Espresso|Latte|Cappuccino \n")
-    choice.lower()
     #If the choice is report it will return the resources of the machine that are available
-    if choice == 'report':
+    if choice.lower() == 'report':
         print(f"water:{resources["water"]}")
         print(f"milk: {resources["milk"]}")
         print(f"coffee: {resources["coffee"]}")
     #if the choice is a type of coffee it will then charge
-    elif choice in {"espresso","latte","cappuccino"}:
+    elif choice.lower() in {"espresso","latte","cappuccino"}:
         transaction(choice)
 
 coffee_choices()
