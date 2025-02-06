34acc990e975a912dd1d87d157fd323e80ea4fbe
Sobajer <68204977+Jlep243@users.noreply.github.com~>
2025-02-05 23:12:16 -0700

    You can now see the Library catalogue of books

--- LibraryManagementSystem/Library.py
+++ LibraryManagementSystem/Library.py
@@ -16,21 +16,30 @@ books = [
 class Library:
     def library_options(key,username):
         print(f"welcome to the library")
-        options = input("1.Search for book, 2.Put book on hold, 3.Borrow book, 4.Return book: ")
+        options = input("1.Search for book, 2.Put book on hold, 3.Borrow book, 4.Return book, 5.List of Books: ")
         if options == "1":
             author_or_title = input("would you like to search by author or title? ")
             Library.search_for_book(author_or_title)
+
+        # put books on hold
         # elif options == "2":
-            
+        
+        # borrow book
         # elif options == "3":
+        
+        # Return book 
+        # elif options == "4"
+
+        #shows list of books
+        elif options == "5":
+            Library.list_of_books()
 
     def search_for_book(author_or_title):
         if author_or_title == "title":
             title = input("search for title: ")
-            print("   ")
+            print("\n")
             books_found = []
             a = 0
-            b = 0
             for i in range(len(books)):
                 book_title = books[i]["title"]
                 book_author = books[i]["author"]
@@ -39,7 +48,6 @@ class Library:
                     books_found.append(book_author)
             for book in books_found:
                 a += 1
-                b += 1
                 if a % 2 != 0:
                     print(f"Title:{book}")
                 elif a % 2 == 0:
@@ -48,6 +56,7 @@ class Library:
 
         elif author_or_title == "author":
             author = input("search by author: ")
+            print("\n")
             books_found=[]
             a = 0
             for i in range(len(books)):
@@ -64,4 +73,9 @@ class Library:
                     print(f"Author:{book} \n")
             Library.library_options(key=True,username="jack")
         
-        
+    def list_of_books():
+        print("\n")
+        for book in books:
+            print(f"Title: {book["title"]}, Author: {book["author"]}, CheckedOut: {book["is_borrowed"]}")
+        print("\n")
+        Library.library_options(key=True,username="jack")
