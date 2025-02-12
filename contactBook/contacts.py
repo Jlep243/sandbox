a211e54626ac012501531b7ab3c73d0fbeb7bee9
Jerry <68204977+Jlep243@users.noreply.github.com>
2025-02-11 19:05:33 -0700

    Added new project to practice using vim

---
+++ contactBook/contacts.py
@@ -0,0 +1,7 @@
+contacts = [{"name":"karl","email":"forKarl@gmail.com","address":"copenhagen,Denmark"},
+            {"name":"shamwow","email":"shammyWow@yahoo.com","address":"shamwow,stree"},
+            {"name": "Alice Johnson", "phone": "555-1234", "email": "alice@example.com"},
+            {"name": "Bob Smith", "phone": "555-5678", "email": "bob@example.com"},
+            {"name": "Charlie Brown", "phone": "555-8765", "email": "charlie@example.com"},
+            {"name": "Dana White", "phone": "555-4321", "email": "dana@example.com"},
+            {"name": "Eve Adams", "phone": "555-6789", "email": "eve@example.com"}]
---
+++ contactBook/main.py
@@ -0,0 +1,9 @@
+from contacts import contacts
+
+option =  input("1.Look up|2.view all contacts|3.Update contact|4.Add contact|5.Remove contact:")
+if option == "1":
+    print("hello world")
+elif option == "2":
+    for contact in contacts:
+        print(f"{contact["name"]}| email: {contact["email"]}|")
+
