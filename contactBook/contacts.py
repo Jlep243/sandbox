de1932a9da9cd57a3596c095d3dbb7bb17e6e2b6
Sobajer <68204977+Jlep243@users.noreply.github.com>
2025-02-13 10:08:45 -0700

    Can now add new contacts and exit the program

--- contactBook/contacts.py
+++ contactBook/contacts.py
@@ -1,9 +1,20 @@
 contacts = [{"name":"karl","email":"forKarl@gmail.com","address":"copenhagen,Denmark","phone":"929-3421"},
             {"name":"shamwow","email":"shammyWow@yahoo.com","address":"shamwow,street","phone":"902-0921"},
-            {"name": "Alice Johnson", "phone": "555-1234", "email": "alice@example.com","phone":"341-9092"},
+            {"name": "Alice Johnson", "phone": "555-1234", "email": "alice@example.com"},
             {"name": "Bob Smi", "phone": "555-5678", "email": "bob@example.com"},
             {"name": "Charlie Brown", "phone": "555-8765", "email": "charlie@example.com"},
             {"name": "Dana White", "phone": "555-4321", "email": "dana@example.com"},
             {"name": "Eve Adams", "phone": "555-6789", "email": "eve@example.com"}]
 
 
+class Contact:
+	
+	def __init__(self,name,phone,email,address):
+	    self.name = name
+	    self.phone = phone
+	    self.email = email
+	    self.address = address
+	    contacts.append({"name":self.name,"phone":self.phone,"email":self.email,"address":self.address})
+	    print(f"{self.name} has been added to contacts")
+
+Contact('pepe','678-291-5423','pepe@gmail.com','1543 East 450 South')	
--- contactBook/main.py
+++ contactBook/main.py
@@ -1,19 +1,28 @@
 from contacts import contacts
-option =  input("1.Look up|2.view all contacts|3.Update contact|4.Add contact|5.Remove contact:")
-if option == "1":
-    #while True:
-    lookupOption = input("Would you like to lookup by phone, email, address, or name: ")
-    identifier = input(f"{lookupOption}: ")
-    for contact in contacts:
-        if contact[lookupOption] == identifier:
-            print(f"\n name: {contact["name"]}| phone: {contact["phone"]}| email: {contact["email"]}| address: {contact["address"]}")	 
-elif option== "2":
-    for contact in contacts:
-        print(f"\n{contact["name"]}|\n phone:{contact["phone"]}|\n email: {contact["email"]}")
-if option == "3":
-	person = input("which contact would you like to update:")
+from contacts import Contact
 
-#for contact in contacts:
-#    print(contact["name"])
-    
+while True:
+    option =  input("1.Look up|2.view all contacts|3.Update contact|4.Add contact|5.Remove contact|6.exit: ")
 
+    if option == "1":
+        lookupOption = input("Would you like to lookup by phone, email, address, or name: ")
+        identifier = input(f"{lookupOption}: ")
+        for contact in contacts:
+            if contact[lookupOption] == identifier:
+                print(f"\n name: {contact["name"]}| phone: {contact["phone"]}| email: {contact["email"]}| address: {contact["address"]}")    
+
+    elif option== "2":
+        for contact in contacts:
+            print(f"\n{contact["name"]}|\n phone:{contact["phone"]}|\n email: {contact["email"]}")
+
+    elif option == "3":
+        person = input("which contact would you like to update:")
+
+    elif option == "4":
+        name = input(" name: ")
+        phone = input(" phone: ")
+        email = input(" email: ")
+        address = input(" address: ")
+        Contact(name,phone,email,address)
+    elif option.lower() == "exit":
+        exit() 
