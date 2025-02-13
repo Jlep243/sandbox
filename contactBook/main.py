03987c64548052b9007cd123dfce12014205f9b5
Jerry <68204977+Jlep243@users.noreply.github.com>
2025-02-12 18:27:48 -0700

    lookup by email,phone,or number should work.

--- contactBook/main.py
+++ contactBook/main.py
@@ -1,12 +1,20 @@
 from contacts import contacts
-
 option =  input("1.Look up|2.view all contacts|3.Update contact|4.Add contact|5.Remove contact:")
 if option == "1":
-    person = input("Would you like to lookup by phone, email, address, or name?")
-	if contact[input] ==
-elif option == "2":
+    #while True:
+    lookupOption = input("Would you like to lookup by phone, email, address, or name: ")
+    identifier = input(f"{lookupOption}: ")
+    for contact in contacts:
+        if contact[lookupOption] == identifier:
+            print(contact)
+		 
+elif option== "2":
     for contact in contacts:
         print(f"\n{contact["name"]}|\n phone:{contact["phone"]}|\n email: {contact["email"]}")
-elif option == "3":
-	person = input("which contact would you like to update:")
+if option == "3":
+ 	person = input("which contact would you like to update:")
+
+#for contact in contacts:
+#    print(contact["name"])
+    
 
