808e7422a955213e861a779db4118109a2bb9bae
Jerry <68204977+Jlep243@users.noreply.github.com>
2025-02-18 16:50:37 -0700

    got it to work on laptop and changed pensize

--- RandomWalk/main.py
+++ RandomWalk/main.py
@@ -4,12 +4,18 @@ from turtle import *
 tim = Turtle()
 
 def random_walk():
-
+    tim.pensize(10)
     tim.color("blue")
     tim.forward(1000000)
 
+def square():
+    tim.pensize(100)
+    tim.color("red")
+    for _ in range(4):
+        tim.forward(100)
+        tim.right(90)
 
-random_walk()
-
+#random_walk()
+square()
 screen = Screen()
 screen.exitonclick()
