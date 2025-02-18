313ac564cc33c42d8f2f2d2f924862e3dcd0b34a
Sobajer <68204977+Jlep243@users.noreply.github.com>
2025-02-18 13:51:28 -0700

    added random walk project

---
+++ RandomWalk/main.py
@@ -0,0 +1,15 @@
+import random
+from turtle import *
+
+tim = Turtle()
+
+def random_walk():
+
+    tim.color("blue")
+    tim.forward(1000000)
+
+
+random_walk()
+
+screen = Screen()
+screen.exitonclick()
