6f5263a3f05222d302d03ae467b279a31d085706
Jerry <68204977+Jlep243@users.noreply.github.com>
2025-02-20 21:45:29 -0700

    movement added for a and d keys

--- Etch-a-sketch/main.py
+++ Etch-a-sketch/main.py
@@ -2,8 +2,21 @@ import turtle as t
 
 tom = t.Turtle()
 
-tom.pensize(12)
-tom.forward(100)
+def right():
+    tom.pensize(5)
+    tom.forward(1)
+
+def left():
+    tom.pensize(5)
+    tom.left(1)
+
+t.delay(0)
+
+t.speed("fastest")
+t.onkey(left,"a")
+t.onkey(right,"d")
+t.listen()
 
 
-t.exitonclick()
+
+t.done()
