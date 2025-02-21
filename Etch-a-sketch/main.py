9c23177536db4df8164ccf04d39f787c0b383c56
Sobajer <68204977+Jlep243@users.noreply.github.com>
2025-02-21 14:36:48 -0700

    made the arrow faster and controls work.

--- Etch-a-sketch/main.py
+++ Etch-a-sketch/main.py
@@ -1,22 +1,30 @@
 import turtle as t
+win =t.Screen()
 
 tom = t.Turtle()
+tom.fillcolor("red")
+tom.speed("fastest")
+tom.screen.delay(0)
+tom.pensize(5)
+
 
 def right():
-    tom.pensize(5)
-    tom.forward(1)
-
+    tom.speed(0)
+    tom.forward(5)
+    
 def left():
-    tom.pensize(5)
-    tom.left(1)
+    tom.back(5)
+    
+def turn_up():
+    tom.left(5)
 
-t.delay(0)
+def turn_down():
+    tom.right(5)
 
-t.speed("fastest")
-t.onkey(left,"a")
-t.onkey(right,"d")
-t.listen()
+win.onkeypress(turn_down,"Down")
+win.onkeypress(turn_up,"Up")
+win.onkey(left,"a")
+win.onkey(right,"d")
+win.listen()
 
-
-
-t.done()
+win.mainloop()
