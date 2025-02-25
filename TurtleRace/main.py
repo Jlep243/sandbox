6f52a25b09446ddc0e4eef201e111acaea3b016b
Sobajer <68204977+Jlep243@users.noreply.github.com>
2025-02-25 01:30:47 -0700

    Figured out a solution for the turtles to not be the same color.

--- TurtleRace/main.py
+++ TurtleRace/main.py
@@ -2,7 +2,7 @@ import turtle as t
 import random
 
 win =t.Screen()
-t.bgcolor("dark blue")
+t.bgcolor("light blue")
 
 tim = t.Turtle()
 tom = t.Turtle()
@@ -11,21 +11,23 @@ tommy = t.Turtle()
 
 racers = [tim,tom,timmy,tommy]
 
-colors = ["red","light blue","black","yellow","pink","purple"]
+colors = ["red","blue","black","yellow","pink","purple"]
 number_of_colors = 5
 
 def rand_color():
-    random_color = colors[random.randint(0,5)]
+    random_color = random.choice(colors)
     return random_color
 
 def racemode():
     i = 0
     for racer in racers:
+        color = rand_color()
+        racer.color(color)
+        colors.remove(color)
         racer.shape('turtle')
         racer.penup()
         i+=50
         racer.setpos(0,i)
-        racer.color(rand_color()) 
 
 racemode()
 
