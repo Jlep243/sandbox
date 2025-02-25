858b56d2007b4d1aed16facec7990e6d82a1aa3b
Jerry <68204977+Jlep243@users.noreply.github.com>
2025-02-25 15:44:37 -0700

    implemented code for turtles to race, but need to implement finish line.

--- TurtleRace/main.py
+++ TurtleRace/main.py
@@ -3,6 +3,7 @@ import random
 
 win =t.Screen()
 t.bgcolor("light blue")
+win.screensize(100,100)
 
 tim = t.Turtle()
 tom = t.Turtle()
@@ -31,10 +32,11 @@ def racesetup():
         racer.setpos(0,i)
 
 def racestart():
-    win.textinput("Place your bets!",f" Which turtle do you think will win? \n Tim: {tim.pencolor()}, Tom: {tom.pencolor()}, Timmy: {timmy.pencolor()}, Tommy: {tommy.pencolor()}")
-    for racer in racers:
-        racer.forward(random.choice(speed))
-
+    bets = win.textinput("Place your bets!",f" Which turtle do you think will win? \n Tim: {tim.pencolor()}, Tom: {tom.pencolor()}, Timmy: {timmy.pencolor()}, Tommy: {tommy.pencolor()}")
+    while tim.ycor()!=100 or tom.ycor()!=100 or tommy.ycor()!=100 or timmy.ycor()!=100:
+        for racer in racers:
+            racer.forward(random.choice(speed)) 
+            return racer
 racesetup()
 racestart()
 t.mainloop() 
