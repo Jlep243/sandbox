79778192e98c5e75a081d94e94cd1e68949b125d
Sobajer <68204977+Jlep243@users.noreply.github.com>
2025-02-25 02:55:00 -0700

    got prompt working and sentences are better arranged.

--- TurtleRace/main.py
+++ TurtleRace/main.py
@@ -10,7 +10,7 @@ timmy = t.Turtle()
 tommy = t.Turtle()
 
 racers = [tim,tom,timmy,tommy]
-
+speed = [5,10,15,20,25,30,35]
 colors = ["red","blue","black","yellow","pink","purple"]
 number_of_colors = 5
 
@@ -18,17 +18,23 @@ def rand_color():
     random_color = random.choice(colors)
     return random_color
 
-def racemode():
+def racesetup():
     i = 0
     for racer in racers:
         color = rand_color()
         racer.color(color)
         colors.remove(color)
+
         racer.shape('turtle')
         racer.penup()
         i+=50
         racer.setpos(0,i)
 
-racemode()
+def racestart():
+    win.textinput("Place your bets!",f" Which turtle do you think will win? \n Tim: {tim.color()[0]}, Tom: {tom.color()[1]}, Timmy: {timmy.color()[0]}, Tommy: {tommy.color()[0]}")
+    for racer in racers:
+        racer.forward(random.choice(speed))
 
+racesetup()
+racestart()
 t.mainloop() 
