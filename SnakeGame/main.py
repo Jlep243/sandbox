551c51ed2ec8a16be52aa6a07b731de58ace0d1d
Sobajer <68204977+Jlep243@users.noreply.github.com>
2025-02-26 19:27:08 -0700

    Added snake on screen

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -1,10 +1,16 @@
 from turtle import Screen, Turtle
 
+snake = Turtle()
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.title("My snake game")
 screen.bgcolor("black")
 
+snake_bits = [(0,0), (-20,0), (-40,0)]
 
+for position in snake_bits:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
 
 screen.mainloop()
