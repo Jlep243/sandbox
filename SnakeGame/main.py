cf3527256fd211ee8017b673229c36ecede127e5
Jerry <68204977+Jlep243@users.noreply.github.com>
2025-03-12 23:26:56 -0600

    Changed frames are rendered

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -1,10 +1,26 @@
 from turtle import Screen, Turtle
-
+turtle = Turtle()
 screen = Screen()
 screen.setup(width=600, height=600)
-screen.title("My snake game")
-screen.bgcolor("black")
+screen.bgcolor('black')
+screen.title('My Snake Game')
+screen.tracer(0)
+
+snake_segments = [(0,0),(-20,0),(-40,0)]
+segments = []
+
+for i in snake_segments:
+    new_segment = Turtle("square")
+    new_segment.penup() 
+    new_segment.color("white")
+    new_segment.setpos(i)
+    segments.append(new_segment) 
 
 
-
+while True:
+    for i in segments: 
+        screen.update()
+        i.forward(1)    
+    
 screen.mainloop()
+
