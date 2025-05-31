7a2e23c842e26b2de0ae2e4ead988b18da3719bc
Sobajer <68204977+Jlep243@users.noreply.github.com>
2025-05-30 20:09:38 -0600

    added things?

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen, Turtle
 import time
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,26 +8,29 @@ screen.title("My snake game")
 screen.bgcolor("black")
 screen.tracer(0)
 
-snake_bits = [(0,0),(-20,0),(-40,0)]
-segments = []
+#snake_bits = [(0,0),(-20,0),(-40,0)]
+#segments = []
 
 
-for segment in snake_bits:
-    new_segment = Turtle('square')
-    new_segment.color('white')
-    new_segment.penup()
-    new_segment.goto(segment)
-    segments.append(new_segment)
+#for segment in snake_bits:
+#    new_segment = Turtle('square')
+#    new_segment.color('white')
+#    new_segment.penup()
+#    new_segment.goto(segment)
+#    segments.append(new_segment)
+#
+#
+#game_is_on = True
+#Snake()
 
-game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-
-    for seg_num in range(len(segments)-1,0,-1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x,new_y)
+#while game_is_on:
+#    screen.update()
+#    time.sleep(0.1)
+#
+#    for seg_num in range(len(segments)-1,0,-1):
+#        new_x = segments[seg_num - 1].xcor()
+#        new_y = segments[seg_num - 1].ycor()
+#        segments[seg_num].goto(new_x,new_y)
 
 
 screen.mainloop()
