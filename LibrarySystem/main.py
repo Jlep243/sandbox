ea852721bc812bc1f5c7c83ab6063ae4fe16f0bb
Jerry <68204977+Jlep243@users.noreply.github.com>
2025-10-21 12:13:23 -0600

    added main.py

--- LibrarySystem/main.py
+++ LibrarySystem/main.py
@@ -1,13 +1,24 @@
-logged_in = False
+import operations
 
-def sign_in(logged_in):
-   if logged_in == True:
-        print('welcome to your account!') 
+def main():
+    while True:
+        option = input("|1.Login|2.Search|3.AddBook|4.Exit: ")
+        option = option.lower()  
+        match option:
+            case "1"| "login":
+                user_name = input("username: ")
+                password = input("password: ")
+            case "2"|"search": 
+                book_name = input("book title: ")
+                author = input("Author: ")
+                date = input("Date: ")
+                operations.search_book(book_name,author,date)
+            case "3"|"addbook":
+                operations.add_book() 
+            case "4"|"exit":
+                input("Shutting down.... \n press enter...")
+                break
 
-while logged_in != True:
-    login_or_register = input("Would you like to Login or Register: ")
-    if login_or_register == "login":
-        logged_in = True
-        sign_in(logged_in)
-    else:
-        print('oops')
+
+if __name__ == "__main__":
+    main()
